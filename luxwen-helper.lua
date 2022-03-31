a1ded48f1e36336c75013a49d5b8108c1ddccf52
CerberUSGR0 <99074569+CerberUSGR0@users.noreply.github.com>
2022-03-31 13:46:16 -0400

    Add files via upload

--- luxwen-helper.lua
+++ luxwen-helper.lua
@@ -15,7 +15,7 @@ local script_vers_text = "1.00"
 local update_url = "https://raw.githubusercontent.com/CerberUSGR0/SKRIPT/main/update.ini" -- тут тоже свою ссылку
 local update_path = getWorkingDirectory() .. "/update.ini" -- и тут свою ссылку
 
-local script_url = "https://github.com/thechampguess/scripts/blob/master/autoupdate_lesson_16.luac?raw=true" -- тут свою ссылку
+local script_url = "https://raw.githubusercontent.com/CerberUSGR0/SKRIPT/main/luxwen-helper.lua" -- тут свою ссылку
 local script_path = thisScript().path
 
 local pm = {
